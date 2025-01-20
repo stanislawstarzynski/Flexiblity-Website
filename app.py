67b90c61e574463eeb850c0e62daef9b50b82e34
stanislawstarzynski <stanislaw.starzynski@student.nhlstenden.com>
2025-01-20 20:41:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'flexiblitywebsite.stanislaw@gmail.com'
-app.config['MAIL_PASSWORD'] = 'owme dell fjvr ssgy'
-app.config['MAIL_DEFAULT_SENDER'] = 'flexiblitywebsite.stanislaw@gmail.com'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+
 
 # Initialize extensions
 db = SQLAlchemy(app)
